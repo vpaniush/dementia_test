77c0382e74f8dfb6a3c3df2dd3b3318b4b2aed87
vpaniush <volodymyr2507@gmail.com>
2022-11-23 12:41:59 +0200

    adds network to assets

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'presentation/screens/home/home_screen.dart';
+
+class DDD extends StatelessWidget {
+  const DDD({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('You have pushed the button this many times:'),
+          ],
+        ),
+      ),
+    );
+  }
+}
