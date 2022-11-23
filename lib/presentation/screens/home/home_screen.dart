836943fa6bf4ecbd7b8ef29052653b22575b3c77
vpaniush <volodymyr2507@gmail.com>
2022-11-24 00:22:22 +0200

    testing scribble

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:scribble/scribble.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key, required this.title}) : super(key: key);
@@ -10,20 +12,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late ScribbleNotifier notifier;
+
+  @override
+  void initState() {
+    notifier = ScribbleNotifier(
+      widths: [10],
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('You have pushed the button this many times:'),
-          ],
+        leading: IconButton(
+          icon: const Icon(Icons.save),
+          tooltip: "Save to Image",
+          onPressed: () => _saveImage(context),
         ),
       ),
+      body: Stack(
+        children: [
+          Scribble(
+            notifier: notifier,
+            drawPen: true,
+          ),
+          Positioned(
+            top: 16,
+            right: 16,
+            child: _buildColorToolbar(context),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> _saveImage(BuildContext context) async {
+    final image = await notifier.renderImage();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Your Image"),
+        content: Image.memory(image.buffer.asUint8List()),
+      ),
+    );
+  }
+
+  Widget _buildColorToolbar(BuildContext context) {
+    return StateNotifierBuilder<ScribbleState>(
+      stateNotifier: notifier,
+      builder: (context, state, _) => _buildClearButton(context),
+    );
+  }
+
+  Widget _buildClearButton(BuildContext context) {
+    return FloatingActionButton.small(
+      tooltip: "Clear",
+      onPressed: notifier.clear,
+      disabledElevation: 0,
+      backgroundColor: Colors.blueGrey,
+      child: const Icon(Icons.clear),
     );
   }
 }
